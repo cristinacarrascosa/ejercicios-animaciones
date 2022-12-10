0cb484234e38f9545aa45322368773445d3e4642
cristinacarrascosa <cctausias@gmail.com>
2022-12-10 18:33:57 +0100

    Ejercicios acabados

---
+++ js/script9.js
@@ -0,0 +1,7 @@
+let boton = document.querySelector("#boton");
+console.log(boton);
+boton.addEventListener("click", function () {
+
+    boton.classList.remove('animacion');
+  console.log("Click");
+});
